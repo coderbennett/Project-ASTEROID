2949e3815422fe408fc6a4bb89e44a30c592c6e1
joeymonterey <jbennett@csumb.edu>
2021-03-03 15:47:37 -0800

    start button functionality
    the start button on the main menu will now direct the player to the game properly. this commit also added mouse over animations for the text so it will be obvious which selection you have made or that they are clickable.

---
+++ Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour {
+
+	public Text text;
+	private bool isOver = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOver)
+		{
+			text.color = new Color(1f, 1f, 1f);
+		} else
+        {
+			text.color = new Color(0.75f, 0.75f, 0.75f);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (Input.GetMouseButtonDown(0) && isOver && gameObject.name == "startButton")
+		{
+			SceneManager.LoadScene("Space");
+		}
+
+		if (Input.GetMouseButtonDown(0) && isOver && gameObject.name == "quitButton")
+		{
+			Application.Quit();
+		}
+	}
+
+		public void OnMouseEnter()
+	{
+		isOver = true;
+	}
+
+	public void OnMouseExit()
+	{
+		isOver = false;
+	}
+}
