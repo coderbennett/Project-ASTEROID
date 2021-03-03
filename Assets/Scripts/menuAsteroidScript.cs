36a4af68fc52170da76661929783a1d7e1c3cd32
joeymonterey <jbennett@csumb.edu>
2021-03-03 13:40:27 -0800

    added main menu
    created a main menu. the main menu has the earth & moon orbiting the center of the screen while 8 asteroids bounce around the screen. text appears in the center of the screen for navigating through the game. the functional part of the main menu has not been added yet

---
+++ Assets/Scripts/menuAsteroidScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuAsteroidScript : MonoBehaviour {
+
+	public float speed;
+	public float h;
+	public float v;
+
+	// Use this for initialization
+	void Start () {
+		h = Random.Range(-1f, 1f);
+		v = Random.Range(-1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector2(transform.position.x + (h * speed), transform.position.y + (v * speed));
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "wall")
+        {
+			h *= -1;
+			v *= -1;
+		}
+
+		if (collision.tag == "barrier")
+		{
+			gameObject.transform.position = new Vector2(-0.04f, 5.44f);
+		}
+	}
+}
---
+++ Assets/Scripts/menuSpawnerScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class menuSpawnerScript : MonoBehaviour {
+
+	public GameObject point1;
+	public GameObject point2;
+	public GameObject point3;
+	public GameObject point4;
+	public GameObject goal1;
+	public GameObject goal2;
+	public GameObject goal3;
+	public GameObject goal4;
+	public GameObject asteroid;
+
+	public float spawnTime = 6f;
+
+	private GameObject point;
+	private GameObject newAsteroid;
+	private int spawner;
+	private bool spawn = false;
+	private int goal;
+	private Transform goalTransform;
+
+	// Use this for initialization
+	void Start () {
+
+		spawner = Random.Range(1, 5);
+		if (spawner == 1)
+		{
+			point = point1;
+			goalTransform = goal2.transform;
+		}
+		else if (spawner == 2)
+		{
+			point = point2;
+			goalTransform = goal1.transform;
+		}
+		else if (spawner == 3)
+		{
+			point = point3;
+			goalTransform = goal4.transform;
+		}
+		else
+		{
+			point = point4;
+			goalTransform = goal3.transform;
+		}
+		Invoke("SpawnAsteroid", spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (spawn == true)
+		{
+			spawner = Random.Range(1, 5);
+			if (spawner == 1)
+			{
+				point = point1;
+				goalTransform = goal2.transform;
+			}
+			else if (spawner == 2)
+			{
+				point = point2;
+				goalTransform = goal1.transform;
+			}
+			else if (spawner == 3)
+			{
+				point = point3;
+				goalTransform = goal4.transform;
+			}
+			else
+			{
+				point = point4;
+				goalTransform = goal3.transform;
+			}
+			Invoke("SpawnAsteroid", spawnTime);
+			spawn = false;
+		}
+
+	}
+
+	void SpawnAsteroid()
+	{
+		newAsteroid = Instantiate(asteroid, point.transform.position, Quaternion.identity);
+		menuAsteroidScript asteroidController = (menuAsteroidScript)newAsteroid.GetComponent(typeof(menuAsteroidScript));
+		asteroidController.SetDirection(goalTransform);
+	}
+}
+*/
